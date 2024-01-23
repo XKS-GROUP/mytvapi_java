c73d5bbb9edb5fb0b9a930c67c8ac595a4a65346
Smeth <kanasmeth@gmail.com>
2024-01-23 20:43:36 +0300

    Init Project

---
+++ src/main/java/com/mytv/api/MytvapiApplication.java
@@ -0,0 +1,13 @@
+package com.mytv.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytvapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytvapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytv/api/MytvapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytv.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytvapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
