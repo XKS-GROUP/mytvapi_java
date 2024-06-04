cdbef70a1b3f60fc47e18701e412a05a8e575c81
smeth <kanasmeth@gmail.com>
2024-06-04 12:36:46 +0300

    @service dans settings service

--- src/main/java/com/mytv/api/service/util/SmtpService.java
+++ src/main/java/com/mytv/api/service/util/SmtpService.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.mytv.api.model.util.SmtpSetting;
 import com.mytv.api.repository.smtpRepository;
 
+@Service
 public class SmtpService {
 
 	
