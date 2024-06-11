ad26ebf3cd1487f230eeef673dbcca7b2a461233
smeth <kanasmeth@gmail.com>
2024-06-11 19:24:11 +0300

    MAJ model Setting

--- src/main/java/com/mytv/api/model/util/FirebaseSetting.java
+++ src/main/java/com/mytv/api/model/util/FirebaseSetting.java
@@ -8,11 +8,13 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 public class FirebaseSetting {
 	
 	@Id
--- src/main/java/com/mytv/api/model/util/R2cloudSetting.java
+++ src/main/java/com/mytv/api/model/util/R2cloudSetting.java
@@ -6,11 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class R2cloudSetting {
 
 	@Id
