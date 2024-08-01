e54fba1c67ca80990b56215d431798c657712eef
smeth <kanasmeth@gmail.com>
2024-08-01 09:45:59 +0100

    correction fichier retour Jeff

--- src/main/java/com/mytv/api/aws/FolderService.java
+++ src/main/java/com/mytv/api/aws/FolderService.java
@@ -4,6 +4,7 @@ package com.mytv.api.aws;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -14,10 +15,23 @@ public class FolderService {
 	@Autowired
 	FolderRopository rep;
 	
+    @Value("${aws.s3.bucket.name}")
+    private String bucketName;
+    
+    @Value("${aws.s3.accespoint}")
+    private String accesspoint;
+	
 	
 	public Folder create(Folder folder) {
 		
-		folder.setFolderpath(null);
+		if(folder.getFolderpath().isEmpty()) {
+			
+			folder.setFolderpath(accesspoint);
+		}
+		else {
+			folder.setFolderpath(accesspoint+"/"+folder.name);
+		}
+		
 		return rep.save(folder);
 		
 	}
