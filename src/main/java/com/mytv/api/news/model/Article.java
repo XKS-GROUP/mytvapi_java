034839d2f042216505fe4801ebbddcf5fdbaf91e
smeth <kanasmeth@gmail.com>
2024-08-22 12:12:53 +0100

    Article Ajout de la propriete media, overview, lis_categ

--- src/main/java/com/mytv/api/news/model/Article.java
+++ src/main/java/com/mytv/api/news/model/Article.java
@@ -3,11 +3,15 @@ package com.mytv.api.news.model;
 import java.sql.Date;
 import java.util.List;
 
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -29,6 +33,9 @@ public class Article {
 	@Column(nullable = false, columnDefinition = "TEXT")
 	String title;
 	
+	@Column(nullable = false, columnDefinition = "TEXT")
+	String overview;
+	
 	@NotBlank(message = "Un cover est requise")
 	@Column(nullable = false, columnDefinition = "TEXT")
 	String cover;
@@ -44,5 +51,14 @@ public class Article {
 	Long idUser;
 	
 	List<Long> categories;
+	
+	@Column(nullable = false, columnDefinition = "boolean default false")
+	boolean status;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "id",  cascade = CascadeType.ALL)
+	List<CategArticle> list_categorie;
+	
+	@Column(nullable = false, columnDefinition = "TEXT")
+	String media;
 	
 }
